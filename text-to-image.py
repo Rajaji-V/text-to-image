c92c237fd0cf4b70d6d6bd076e33d019fd4e2469
Rajaji-V <kit27.ad044@gmail.com>
2025-07-28 20:41:13 +0530

    Add files via upload

---
+++ text-to-image.py
@@ -0,0 +1,12 @@
+from diffusers import StableDiffusionPipeline
+import torch
+from PIL import Image
+
+pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4")
+pipe = pipe.to("cuda" if torch.cuda.is_available() else "cpu")
+prompt = "a castle floating in the sky during sunset, digital art"
+
+image = pipe(prompt).images[0]
+image.show()
+image.save("generated_image.png")
+print("Image saved as generated_image.png")

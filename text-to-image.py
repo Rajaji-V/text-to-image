25c107b6ee60f14903b344a8506b400fb6d588dc
Rajaji-V <kit27.ad044@gmail.com>
2025-08-02 09:29:19 +0530

    Update text-to-image.py

--- text-to-image.py
+++ text-to-image.py
@@ -4,9 +4,10 @@ from PIL import Image
 
 pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4")
 pipe = pipe.to("cuda" if torch.cuda.is_available() else "cpu")
-prompt = "a castle floating in the sky during sunset, digital art"
+prompt = input()
 
 image = pipe(prompt).images[0]
 image.show()
 image.save("generated_image.png")
 print("Image saved as generated_image.png")
+
